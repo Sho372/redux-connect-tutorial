50cac71da3c14eac79d6084a430f4bd47528aeea
Shohei.Mizuno <32non23ca@gmail.com>
2025-04-11 18:56:59 -0700

    added todo app for redux connect tutorial

---
+++ src/TodoApp.js
@@ -0,0 +1,24 @@
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import VisibilityFilters from "./components/VisibilityFilters";
+export const VISIBILITY_FILTERS = {
+  ALL: "all",
+  COMPLETED: "completed",
+  INCOMPLETE: "incomplete"
+};
+
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div className="todo-app">
+        <h1>Todo App</h1>
+        <AddTodo />
+        <TodoList />
+        <VisibilityFilters />
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
---
+++ src/components/AddTodo.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+
+  updateInput = (input) => {
+    this.setState({ input });
+  };
+
+  handleAddTodo = () => {
+    console.log('add Todo')
+    // dispatches actions to add todo
+    // sets state back to empty string
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={(e) => this.updateInput(e.target.value)}
+          value={this.state.input}
+        />
+        <button className="add-todo" onClick={this.handleAddTodo}>
+          Add Todo
+        </button>
+      </div>
+    );
+  }
+}
+
+export default AddTodo;
