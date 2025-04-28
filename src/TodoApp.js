a629f7332b53bf2940005920124ecb7383f41f71
Shohei.Mizuno <32non23ca@gmail.com>
2025-04-27 19:44:47 -0700

    スタイルシートをTodoAppにインポート

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.css";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import VisibilityFilters from "./components/VisibilityFilters";
